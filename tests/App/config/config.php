934c619868be82b38ce562c14fb2d839464ac93b
Cristoforo Cervino <cristoforocervino@192.168.1.59>
2026-02-13 16:45:30 +0100

    update ci

--- tests/App/config/config.php
+++ tests/App/config/config.php
@@ -2,22 +2,23 @@
 
 declare(strict_types=1);
 
+use Composer\InstalledVersions;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\HttpKernel\Kernel;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->parameters()
         ->set('kernel.secret', 'test_secret')
         ->set('locale', 'en');
 
+    $frameworkBundleVersion = InstalledVersions::getVersion('symfony/framework-bundle');
     $frameworkConfig = [
         'secret' => '%kernel.secret%',
         'test' => true,
     ];
-    if (Kernel::VERSION_ID >= 60100) {
+    if (null !== $frameworkBundleVersion && \version_compare($frameworkBundleVersion, '6.1.0', '>=')) {
         $frameworkConfig['http_method_override'] = false;
     }
-    if (Kernel::VERSION_ID >= 60400) {
+    if (null !== $frameworkBundleVersion && \version_compare($frameworkBundleVersion, '6.4.0', '>=')) {
         $frameworkConfig['handle_all_throwables'] = true;
         $frameworkConfig['php_errors'] = ['log' => true];
     }
@@ -41,7 +42,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             ],
         ],
     ];
-    if (Kernel::VERSION_ID >= 60400) {
+    if (null !== $frameworkBundleVersion && \version_compare($frameworkBundleVersion, '6.4.0', '>=')) {
         $doctrineOrm['mappings']['Fixtures']['type'] = 'attribute';
     }
 
